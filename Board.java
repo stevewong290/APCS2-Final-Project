a3e7135e21106dfcec3b00b376c94da595c36f96
Steve Wong <swong14@stuy.edu>
2017-05-31 12:36:41 -0400

    work

--- Board.java
+++ Board.java
@@ -1,12 +1,29 @@
+import java.util.*;
+
 public class Board{
     int[][] scoreRef;
     Tile[][] gameBoard;
-    private static ArrayList<String> availTiles;
-    private static ArrayList<String> usedTiles;
+    static ArrayList<int> availTiles;
+    static ArrayList<String> usedTiles;
+    static Tile[] bag;
     //Board constructor
     public Board(){
 	scoreRef = new int[15][15];
 	gameBoard = new Tile[15][15];
+    }
+    public void afterUse(ArrayList<String> used){
+	
+	    
+    }
+    public static void clear(ArrayList used){
+	while(used.size() > 0){
+	    used.remove(0);
+	}
+    }
+    public static void main(String[] args){
+	usedTiles = {"A", "B"};
+	clear(usedTiles);
+	
     }
     
 }
