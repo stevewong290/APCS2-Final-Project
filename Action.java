5bc9fbe31db4f0edbe03434caa3c3f921f247cfd
stephanays <stephaniezheng1@gmail.com>
2017-05-30 22:47:43 -0400

    hi

---
+++ Action.java
@@ -0,0 +1,17 @@
+public class Action{
+    int p1Score;
+    int p2Score;
+    int p3Score;
+    int p4Score;
+    
+    public Action(){
+
+    }
+
+    public makeBag(String filename){
+	try{
+	    File text= new File(filename);
+	    Scanner data = new Scanner(text);
+
+	    
+}
