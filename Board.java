7f31ff5e29348a78d0599153d7b73be46b105133
Steve Wong <swong14@stuy.edu>
2017-05-31 12:19:25 -0400

    added to board.java

--- Board.java
+++ Board.java
@@ -1,7 +1,9 @@
 public class Board{
     int[][] scoreRef;
     Tile[][] gameBoard;
-
+    private static ArrayList<String> availTiles;
+    private static ArrayList<String> usedTiles;
+    //Board constructor
     public Board(){
 	scoreRef = new int[15][15];
 	gameBoard = new Tile[15][15];
