61b66fbb05d1bdb7568f605c52ff38eae9907c27
stephanays <stephaniezheng1@gmail.com>
2017-06-02 12:32:38 -0400

    updated variable types, makeBag, draw, and updateBag is done

--- Action.java
+++ Action.java
@@ -2,13 +2,13 @@ import java.util.*;
 import java.io.*;
 public class Action{
     String[][] tileBag;
-    Tile[] bag;
+    ArrayList<Tile> bag;
 
     public Action(){
     }
 
     public Tile getBag(int index){
-	return bag[index];
+	return bag.get(index);
     }
     
     public void readBag(String filename){
@@ -31,18 +31,46 @@ public class Action{
     }
 
     public void makeBag(){
-	bag=new Tile[100];
+	bag=new ArrayList<Tile>();
 	int index=0;
 	for(int r=0; r<27; r++){
 	    Tile t = new Tile(tileBag[r][0], Integer.parseInt(tileBag[r][1]));
 	    for(int x = Integer.parseInt(tileBag[r][2]); x>0; x--){
-		bag[index]=t;
+		bag.add(t);
 		index++;
 	    }
 	}
 	
     }
-		    
+
+    public void draw(ArrayList<Tile> hand){
+	int num = 7 - (hand.size());
+	if(num == 0){
+	    return;
+	}else{
+	    for(int x = 0; x<num; x++){
+		int index = (int)(Math.random() * (bag.size()));
+		Tile t = bag.get(index);
+		hand.add(t);
+		bag.remove(index);
+	    }
+	}
+    }
+	
+
+    public void updateBag(ArrayList<Tile> tilesDrawn){
+	int num=0;
+	for(int x=0; x<tilesDrawn.size(); x++){
+	    for(int y=0; y<bag.size() && num==0; y++){
+		if(tilesDrawn.get(x).getLetter().equals(bag.get(y).getLetter())){
+		    bag.remove(y);
+		    num=1;
+		}
+	    }
+	    num=0;
+	}
+		
+    }
 		    
 
     public String toString(){
@@ -58,8 +86,16 @@ public class Action{
 
     public String toStringTile(){
 	String str="";
-	for(int x=0; x<bag.length; x++){
-	    str+=bag[x].getLetter()+" ";
+	for(int x=0; x<bag.size(); x++){
+	    str+=bag.get(x).getLetter()+" ";
+	}
+	return str;
+    }
+
+    public String toStringHand(ArrayList<Tile> hand){
+	String str="";
+	for(int x=0; x<hand.size(); x++){
+	    str+=hand.get(x).getLetter()+" ";
 	}
 	return str;
     }
@@ -70,10 +106,32 @@ public class Action{
 	test.readBag("tileBag.txt");
 	System.out.println(test.toString());
 	test.makeBag();
-	//test.toStringTile();
-	Tile x = test.getBag(98);
-	System.out.println(x.getLetter());
-	
+	System.out.println(test.toStringTile());
+	//Tile x = test.getBag(98);
+	//System.out.println(x.getLetter());
+	Tile a = new Tile("A",1);
+	Tile q = new Tile("Q",10);
+	Tile s = new Tile("S",1);
+	Tile v = new Tile("V",4);
+	Tile qu = new Tile("?",0);
+	ArrayList<Tile> used = new ArrayList<Tile>();
+	used.add(a);
+	used.add(q);
+	used.add(s);
+	used.add(v);
+	used.add(qu);
+	test.updateBag(used);
+	System.out.println(test.toStringTile());
+	ArrayList<Tile> hand1 = new ArrayList<Tile>();
+	hand1.add(a);
+	hand1.add(q);
+	hand1.add(s);
+	hand1.add(v);
+	hand1.add(qu);
+	System.out.println(test.toStringHand(hand1));
+	test.draw(hand1);
+	System.out.println(test.toStringHand(hand1));
+	System.out.println(test.toStringTile());
     }
 
 	    
