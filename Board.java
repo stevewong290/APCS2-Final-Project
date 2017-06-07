5117a82308bd96486566cd52e1133e6fe5bdf27f
Steve Wong <stevewong@Steves-iMac.home>
2017-06-06 21:08:16 -0400

    hallelujah, i added the reader to make the special tile board

--- Board.java
+++ Board.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Board{
     int[][] scoreRef;
     Tile[][] gameBoard;
+    int[][] specialBoard;
     static ArrayList<Integer> availTiles = new ArrayList<Integer>();
     static ArrayList<String> usedTiles = new ArrayList<String>();
     static Tile[] bag;
@@ -12,8 +13,42 @@ public class Board{
     public Board(){
 	scoreRef = new int[15][15];
 	gameBoard = new Tile[15][15];
+	specialBoard = new int[15][15];
     }
 
+    public void specialReader(String filename){
+	try{
+	    File text = new File(filename);
+	    Scanner infile = new Scanner(text);
+	    //	    int count = 0;
+	    while(infile.hasNextLine()){
+		String line = infile.nextLine();
+		if(line.substring(1,2).equals(" ")){
+		    if(line.substring(4,5).equals(" ")){
+			specialBoard[Integer.parseInt(line.substring(0,1))][Integer.parseInt(line.substring(3,4))] = Integer.parseInt(line.substring(6));
+		    }
+		    else {
+			specialBoard[Integer.parseInt(line.substring(0,1))][Integer.parseInt(line.substring(3,5))] = Integer.parseInt(line.substring(6));
+		    }
+									    
+		}
+		else{
+		    if(line.substring(4,5).equals(" ")){
+			specialBoard[Integer.parseInt(line.substring(0,2))][Integer.parseInt(line.substring(3,4))] = Integer.parseInt(line.substring(6));
+		    }
+		    else {
+			specialBoard[Integer.parseInt(line.substring(0,2))][Integer.parseInt(line.substring(3,5))] = Integer.parseInt(line.substring(6));
+		    }
+		}
+		//	count++;
+		//	System.out.println(count);
+	    }
+	}
+	catch(FileNotFoundException e){
+	    System.out.println("Missing File");
+	}
+    }
+    
     public static void reader(String filename){
 	try{
 	    File text = new File(filename);
@@ -29,8 +64,12 @@ public class Board{
 	    System.out.println("Missing File");
 	}
     }
-    public static Tile[][] get(){
+    public Tile[][] get(){
 	return gameBoard;
+    }
+
+    public int[][] arrget(){
+	return specialBoard;
     }
     public static void afterPlay(ArrayList<String> used){
 	char c = 'A';
@@ -56,8 +95,24 @@ public class Board{
 	}
 	return ans;
     }
+
+    public static String arrayString(int[][] arr){
+	String ans = "";
+	for(int r = 0; r < 15; r++){
+	    for(int c = 0; c < 15; c++){
+		ans = ans + arr[r][c] + " ";
+	    }
+	    ans += '\n';
+	}
+	return ans;
+
+    }
+    
     public static void main(String[] args){
+	Board a = new Board();
 	reader("tileBag2.txt");
+	a.specialReader("SpecialSpot.txt");
+	System.out.println(arrayString(a.arrget()));
 	usedTiles.add("A");
 	usedTiles.add("B");
 	//	availTiles.add(1);
--- Scrabble.java
+++ Scrabble.java
@@ -1,11 +1,14 @@
-public Scrabble{
+public class Scrabble{
     
-    public static BestWord(Board a; Player b){
-	
+    public String BestWord(Board a, Player b){
+	return " ";
     }
 
 
 
 
-
+    public static void main(String[] args){
+	String a = "0 0";
+	System.out.println(a.substring(1,2));
+    }
 }
