007dae0e3cd13e90849182d1707cd66f89151b39
Steve Wong <swong14@stuy.edu>
2017-06-08 14:01:49 -0400

    trying to work out a good algorithm for spot checker

--- Scrabble.java
+++ Scrabble.java
@@ -4,6 +4,7 @@ public class Scrabble{
     static ArrayList<Integer> xcor = new ArrayList<Integer>();
     static ArrayList<Integer> ycor = new ArrayList<Integer>();
     static ArrayList<Integer> dir  = new ArrayList<Integer>();
+    static ArrayList<Integer> val  = new ArrayList<Integer>();
     //1000 = left to right, 200 = right to left, 30 = bottom to top, 4 = top to bottom
     
     public String BestWord(Board a, Player b){
@@ -16,14 +17,44 @@ public class Scrabble{
 	}
     }
 
-    public static void spotRanker(ArrayList<Integer> x, ArrayList<Integer> y, ArrayList<Integer> direction, Board a){
+    /*  public static void spotRanker(ArrayList<Integer> x, ArrayList<Integer> y, ArrayList<Integer> direction, Board a){
 	int[][] specspot = a.arrget();
 	Tile[][] BOARD = a.get();
 	//after rank spots, it clears the ArrayList
 	clearInt(x);
 	clearInt(y);
 	clearInt(direction);
+	}*/
+
+    public static void ranker(int[][] specspotboard, int x, int y){
+	int value = 0;
+	if(specspotboard[x][y] == 5){
+	    value = 4;
+	}
+	if(specspotboard[x][y] == 6){
+	    value = 3;
+	}
+	if(specspotboard[x][y] == 2){
+	    value = 2;
+	}
+	if(specspotboard[x][y] == 3){
+	    value = 1;
+	}
+	if(value == 0){
+	    value = 5;
+	}
+	return value;
     }
+
+    public static int compare(int x, int y){
+	if(x > y){
+	    return y;
+	}
+	else{
+	    return x;
+	}
+    }
+	
 
     public static void findSpot(Tile[][] board){
 	int posval = 0;
